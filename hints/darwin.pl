0dd06698ac261494210ebe432ff582a2592ef254
Kang-min Liu <gugod@gugod.org>
2020-01-16 13:03:23 +0900

    Remove those version-specific "-isysroot" options.
    With recent versions of xcode, this seems to be handled transparently.
    There seems to be no need to manually specify the path of MacOSX SDKs.
    
    Manually tested on a macOS 10.13.6 (High Sierra), with:
    
    - /usr/bin/perl (5.18.2)
    - /usr/local/bin/perl (homebrew, 5.30.0)
    - a perl-5.18.2 installed done via perlbrew

--- hints/darwin.pl
+++ hints/darwin.pl
@@ -8,27 +8,14 @@ if ( $Config{myarchname} =~ /i386/ ) {
     # Match arch options with the running perl
     if ( my @archs = $Config{ccflags} =~ /-arch ([^ ]+)/g ) {
         $arch = join( '', map { "-arch $_ " } @archs );
-        
         if ( -e 'MANIFEST.SKIP' ) {
             # XXX for development, use only one arch to speed up compiles
             $arch = '-arch x86_64 ';
         }
     }
-    
-    # Read OS version
-    my $sys = `/usr/sbin/system_profiler SPSoftwareDataType`;
-    my ($osx_ver) = $sys =~ /Mac OS X.*(10\.[^ ]+)/;
-    if ( $osx_ver gt '10.5' ) {
-        # Running 10.6+, build as 10.5+
-        $arch .= "-isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5";
-    }
-    else {
-        # 5.8.x, build for 10.3+
-        $arch .= "-isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.3";
-    }
-    
-    print "Adding $arch\n";
-    
+
+    print "Adding $arch\n" if $arch;
+
     my $ccflags   = $Config{ccflags};
     my $ldflags   = $Config{ldflags};
     my $lddlflags = $Config{lddlflags};
